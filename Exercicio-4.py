b517e8c17f8c74e938488a4a041a328be212dcef
Pedro Alferes <alferesx@gmail.com>
2020-02-08 02:11:27 -0300

    Exercise 1,2,3,4,5 Complete

---
+++ Exercicio-4.py
@@ -0,0 +1,7 @@
+valor1 = int(input("Digite o primeiro valor: "))
+valor2 = int(input("Digite o segundo valor: "))
+valor3 = int(input("Digite o terceiro valor: "))
+valor4 = int(input("Digite o quarto valor: "))
+
+media = (valor1 + valor2 + valor3 + valor4)/4
+print('A sua media foi: ',media)
---
+++ Exercicio-5.py
@@ -0,0 +1,4 @@
+print('Conversor de metro para centimetros')
+metro = float(input("Digite a quantidade de metros: "))
+cm = metro * 100
+print("O valor convertido para centimetros é: ",cm)
